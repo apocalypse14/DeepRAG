c41667f9e8e27d36912fee28d41502bc19835224
Ali Azimi <azimi.ali@tum.com>
2025-03-05 19:50:00 +0100

    create simple model to generate text

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 # main files for the project
+
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.2-1B")
+model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-3.2-1B")
+
+
+user_input = input("Enter your text: ")
+
+# Tokenize the input
+inputs = tokenizer(user_input, return_tensors="pt")
+
+# Generate a response
+output = model.generate(**inputs, max_length=50, num_return_sequences=1)
+
+# Decode and print the response
+response = tokenizer.decode(output[0], skip_special_tokens=True)
+print("Response:", response)
